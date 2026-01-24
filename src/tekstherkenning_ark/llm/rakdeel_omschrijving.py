c2a850cdc54e04d9dbe3c3d6404cc2c09d50140e
Sammie <114987952+SammieKn@users.noreply.github.com>
2026-01-24 14:37:39 +0100

    Response format opgesteld obv mapping geert, doornemen van data en bekijken ARK tool code

--- src/tekstherkenning_ark/llm/rakdeel_omschrijving.py
+++ src/tekstherkenning_ark/llm/rakdeel_omschrijving.py
@@ -1,26 +1,56 @@
 from tekstherkenning_ark.llm.llm import AzureOpenAILLM
-from tekstherkenning_ark.enums import MateriaalBovenbouw, MateriaalOnderbouw, MateriaalFundering, MateriaalVloer
+from tekstherkenning_ark.enums import MateriaalBovenbouw, MateriaalOnderbouw, MateriaalFundering, MateriaalVloer, NietBeschikbaar
 from pydantic import BaseModel, Field
 import os
+from typing import Annotated
 
 
-class ConstructionDescription(BaseModel):
-    bouwjaar: int | None
-    lengte_rakdeel: int | None  # TODO: Verplaatsen naar
+class RakdeelOmschrijving(BaseModel):
+    bouwjaar: int | None = Field(gt=1600, lt=2200, default=None, description="Bouwjaar van het rakdeel, houd leeg als onbekend.")
+    
+    # Lengte van het rakdeel, wordt gebruikt om de subdelen te toetsen in lengte tov totaal.
+    lengte_rakdeel: float | None = Field(
+        gt=0.0,
+        default=None,
+        description="Lengte van het rakdeel in meters, houd leeg als onbekend."
+    )
 
-    # benodigd om type te bepalen
-    materiaal_fundering: MateriaalFundering | None  # De fundering is alles onder de vloer, dus damwanden of palen.
-    materiaal_onderbouw: MateriaalOnderbouw | None  # De onderbouw is ...
-    materiaal_bovenbouw: MateriaalBovenbouw | None  # De bovenbouw is het dragende deel van de bovenbouw.
+    # Benodigd om type constructie te bepalen
+    materiaal_fundering: MateriaalFundering | NietBeschikbaar = Field(
+        default=NietBeschikbaar.LEEG,
+        description="Het materiaal van de fundering van het rakdeel. Dit is alles onder de vloer, bijvoorbeeld de palen of damwanden."
+    )
+    materiaal_onderbouw: MateriaalOnderbouw | NietBeschikbaar = Field(
+        default=NietBeschikbaar.LEEG,
+        description="Het materiaal van de onderbouw van het rakdeel. Dit is alles tussen de fundering en de vloer."
+    )
+    materiaal_bovenbouw: MateriaalBovenbouw | NietBeschikbaar = Field(
+        default=NietBeschikbaar.LEEG,
+        description="Het materiaal van de bovenbouw van het rakdeel. Dit is het verticale, dragende deel van de bovenbouw."
+    )
     # benodigd voor onderbouw/fundering
-    materiaal_vloer: MateriaalVloer | None
+    materiaal_vloer: MateriaalVloer | NietBeschikbaar = Field(
+        default=NietBeschikbaar.LEEG,
+        description="Het materiaal van de vloer van het rakdeel."
+    )
 
     # onderloopsheidscherm aanwezig ja/nee
-    onderloopsheidscherm: bool | None
+    onderloopsheidscherm: bool | NietBeschikbaar = Field(
+        default=NietBeschikbaar.LEEG,
+        description="Geef aan of er een onderloopsheidscherm aanwezig is bij dit rakdeel door middel van boolean."
+    )
 
     # benodigd voor constructie waterbodem
-    bovenkant_deksteen_cm: int | None
-    bovenkant_vloer_cm: int | None
+    bovenkant_deksteen_cm: float | None = Field(
+        gt=0.0,
+        default=None,
+        description="De hoogte van de bovenkant van de kademuur in centimeters ten opzichte van de waterlijn. Ook wel kerende hoogte genoemd. De bovenkant wordt vaak aangegeven door deksteen, metselwerk of maaiveld."
+    )
+    bovenkant_vloer_cm: float | None = Field(
+        gt=0.0,
+        default=None,
+        description="De hoogte van de waterlijn tot de bovenkant van de funderingsvloer. Als meerdere elementen onder de waterlijn worden vermeldt, dan tel je die op tot de vloer."
+    )
 
 if __name__ == "__main__":
     api_key = os.environ.get('AZURE_OPENAI_KEY')
