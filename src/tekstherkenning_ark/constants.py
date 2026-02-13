6e814e95ae573d0224daf7fe7fe9aa33c0e0ced4
Sammie <114987952+SammieKn@users.noreply.github.com>
2026-02-13 16:50:27 +0100

    added constants mapped to constructieonderdelen

--- src/tekstherkenning_ark/constants.py
+++ src/tekstherkenning_ark/constants.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 
-
 ROOT_DIR = Path(__file__).parent.parent.parent
 DATA_DIR = ROOT_DIR / "data"
 DUIKRAPPORTEN_DIR = DATA_DIR / "duikrapporten"
@@ -9,3 +8,30 @@ CACHE_DIR = DATA_DIR / ".cache"
 CACHE_DIR.mkdir(parents=True, exist_ok=True)
 
 TEST_PDF_PATH = DUIKRAPPORTEN_DIR / "HEG0801_Houtmonstername&VisueleInspectie_V1.1_20220311.pdf"
+
+CONSTRUCTIEONDERDEEL_MAPPING = {
+    "onderbouw.vloer": [
+        r"vloer",
+    ],
+    "onderbouw": [
+        r"ligger",
+        r"planken",
+        r"kesp",
+        r"deksloof",
+        r"watersloof",
+        r"paal",
+        r"palen",
+        r"schuifhout",
+        r"opsluitklos",
+        r"grondkerend",
+    ],
+    "bovenbouw": [
+        r"metsel",
+        r"deksteen",
+        r"natuursteen",
+        r"wand",
+    ],
+    "onderbouw.onderloopsheidscherm": [
+        r"onderloop",
+    ],
+}
