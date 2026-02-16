7680b73ec6ab2014dc56664f9eee66d1f327fcee
TAVM <matthias.tavasszy@witteveenbos.com>
2026-02-16 19:32:36 +0100

    implement ToestandOnderdeel class

--- src/tekstherkenning_ark/constants.py
+++ src/tekstherkenning_ark/constants.py
@@ -9,29 +9,24 @@ CACHE_DIR.mkdir(parents=True, exist_ok=True)
 
 TEST_PDF_PATH = DUIKRAPPORTEN_DIR / "HEG0801_Houtmonstername&VisueleInspectie_V1.1_20220311.pdf"
 
-CONSTRUCTIEONDERDEEL_MAPPING = {
-    "onderbouw.vloer": [
-        r"vloer",
-    ],
-    "onderbouw": [
-        r"ligger",
-        r"planken",
-        r"kesp",
-        r"deksloof",
-        r"watersloof",
-        r"paal",
-        r"palen",
-        r"schuifhout",
-        r"opsluitklos",
-        r"grondkerend",
-    ],
-    "bovenbouw": [
-        r"metsel",
-        r"deksteen",
-        r"natuursteen",
-        r"wand",
-    ],
-    "onderbouw.onderloopsheidscherm": [
-        r"onderloop",
-    ],
-}
+# Constructieonderdeel mapping
+CONSTRUCTIE_ONDERDELEN_VLOER = ["vloer"]
+CONSTRUCTIE_ONDERDELEN_ONDERBOUW = [
+    "ligger",
+    "planken",
+    "kesp",
+    "deksloof",
+    "watersloof",
+    "paal",
+    "palen",
+    "schuifhout",
+    "opsluitklos",
+    "grondkerend",
+]
+CONSTRUCTIE_ONDERDELEN_BOVENBOUW = [
+    "metsel",
+    "deksteen",
+    "natuursteen",
+    "wand",
+]
+CONSTRUCTIE_ONDERDELEN_ONDERLOOPSHEIDSCHERM = ["onderloop"]
