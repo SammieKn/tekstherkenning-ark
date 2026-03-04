830ee102d069da87bab0201dee4501124ecd0646
TAVM <matthias.tavasszy@witteveenbos.com>
2026-03-04 13:52:44 +0100

    clean up imports

--- src/tekstherkenning_ark/models/rak_base_model.py
+++ src/tekstherkenning_ark/models/rak_base_model.py
@@ -3,17 +3,7 @@ from typing import Type, TypeVar, cast, get_args, get_origin, Union, Annotated, 
 from pydantic import BaseModel, ValidationError
 from pydantic.functional_validators import WrapValidator
 
-from tekstherkenning_ark.models.gebrek import (
-    Gebrek,
-    Scheur,
-    ScheurMetselwerk,
-    ScheurHout,
-    GrondVoerendGat,
-    BuikInWand,
-    Scheefstand,
-    LokaalVerdwenenMetselwerk,
-    OnderloopsheidschermBeschadigd,
-)
+from tekstherkenning_ark.models.gebrek import Gebrek
 from tekstherkenning_ark.models.onverwacht_resultaat import OnverwachtResultaat
 from tekstherkenning_ark.models.toestand_onderdeel import ToestandOnderdeel
 
